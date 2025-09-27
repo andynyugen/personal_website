b485a2a558406595164b9140136ac2c515b488db
Andy Nguyen <andynguyena49@gmail.com>
2025-09-27 16:14:41 -0700

    Added light/dark mode

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect, useState } from 'react';
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
@@ -8,15 +9,37 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(()=> {
+    if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.
+      matchMedia('(prefers-color-scheme:dark').matches)) {
+        setIsDarkMode(true)
+    }else {
+      setIsDarkMode(false)
+    }
+  },[])
+
+  useEffect(()=>{
+    if(isDarkMode){
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    }else{
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = '';
+    }
+  },[isDarkMode])
+
   return (
     <>
-    <Navbar />
-    <Header />
-    <About />
-    <Experience />
-    <Projects />
-    <Contact />
-    <Footer />
+    <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
+    <Header isDarkMode={isDarkMode}/>
+    <About isDarkMode={isDarkMode}/>
+    <Experience isDarkMode={isDarkMode}/>
+    <Projects isDarkMode={isDarkMode}/>
+    <Contact isDarkMode={isDarkMode} />
+    <Footer isDarkMode={isDarkMode}/>
     </>
   );
 }
